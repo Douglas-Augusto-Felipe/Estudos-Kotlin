3a75bcc47621dae490d3a0121da328f09f3437e1
Douglas-Augusto <douglas.full.ti@gmail.com>
2023-10-08 02:53:17 -0300

    primeiro commit

--- src/main/kotlin/estudos/Main.kt
+++ src/main/kotlin/estudos/Main.kt
@@ -1,3 +1,6 @@
-fun main(){
-    println("Hellow Word")
+package estudos // 1. Definição do pacote(algo muito comum para organização de código
+
+fun main(){  // 2. Ponto de entrada de um condigo kotlin, definido pela função[main]
+   //* print("Hello Word")*/ // Obs. Imprime na saída um padrão de texto, sem pular linha
+    println("Hello Word") // 3. Imprime na saída um padrão de texto, pulando linha.
 }
